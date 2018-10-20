7f5f2933bce307a3c1fcf5c6130065d0e569a03a
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2018-10-20 23:40:11 +0700

    Update ArrayDiff.cpp

--- Room Exercise - final/ArrayDiff.cpp
+++ Room Exercise - final/ArrayDiff.cpp
@@ -2,10 +2,13 @@
 
 void uNion(int [], int ,int [], int);
 int main(){
+	//มีสองอาเรย์ จะเอามา ลบ กัน แบบเซต
 	int n1[] = {1,3,9,10,12,15,20};
 	int n2[] = {2,4,9,10,11,15,25,30,50,90};
+	//หาขนาดของอาเรย์ (เป็นสูตร)
 	int len1 = sizeof(n1)/sizeof(n1[0]);
 	int len2 = sizeof(n2)/sizeof(n2[0]);
+	//เข้าฟังก์ชัน
 	uNion(n1,len1,n2,len2);
 	return 0;
 	
@@ -14,6 +17,7 @@ int main(){
 void uNion(int n1[] , int len1,int n2[] , int len2){
 	printf("a-b = ");
 	int i =0, j =0;
+	//หลักการ 
 	while(i<len1 && j<len2){
 		if(n1[i]<n2[j]){
 			printf("%d ",n1[i++]);
